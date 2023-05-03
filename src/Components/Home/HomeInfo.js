4f057ae53a6b3b6e2c88915f9c91c40db53c2976
Codewithdolly <codewithdolly>
2023-05-03 22:27:12 +0530

    page updated.

--- src/Components/Home/HomeInfo.js
+++ src/Components/Home/HomeInfo.js
@@ -2,56 +2,63 @@ import { Button } from "@material-ui/core";
 import React from "react";
 import "./HomeInfo.scss";
 import SaveAltIcon from "@mui/icons-material/SaveAlt";
-import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 
 const HomeInfo = () => {
   return (
     <>
       <div className="homeInfo">
-      <h2 className="homeInfo--skills">My Skills</h2>
-      <p className="homeInfo--border">~~~~~~~~~~~~~~~~~~~~~~~~</p>
+        <h2 className="homeInfo--skills">My Skills</h2>
+        <p className="homeInfo--border">~~~~~~~~~~~~~~~~~~~~~~~~</p>
         <h2 className="homeInfo--information">
-        Hello, I'm Dolly, a front-end developer based in Bangalore, India. I'm passionate about new technologies and developing innovative applications. Also, contribute photos to a community in my free time. 
+          Hello, I'm Dolly, a front-end developer based in Bangalore, India. I'm
+          passionate about new technologies and developing innovative
+          applications. Also, contribute photos to a community in my free time.
         </h2>
         <div className="homeInfo--info">
           <div className="homeInfo--info1">
-          <p>
-          Hello, I'm Dolly, a frontend developer based in Bangalore, India. I'm passionate about working with new technologies and developing innovative applications. I am also patinate for photography, when i find free time  I contribute photos to a comunity called Mixphotos. I have Youtube Channel where I upload my
-                project's video and many more things.
-              </p><br />
-              <p className="homeInfo--info1--p2">
-                Have a Look at My
-                <a
-                  href="https://www.youtube.com/channel/UCD4YW5rhsXi-BK2tzbWVIZA"
-                  style={{ color: "white" }}
-                >
-                  {" "}
-                  Youtube{" "}
-                </a>
-                Channel, It will Students/developer to learn thing in easy ways
-                and help you to make projects. Or you can connect me on
-                Facebook, Linkdin or Instagram. I am always excited to talk
-                about Tech.
-              </p><br />
-<div  className="homeInfo--info--contact--btn">
-            <Button
-              size="large"
-              className="homeInfo--info--contact"
-              href="tel: 6300810401"
-              endIcon={<ArrowRightAltIcon />}
-            >
-              Contact me
-            </Button>
-            <Button
-              variant="outlined"
-              size="large"
-              className="homeInfo--info--CV"
-              href="https://www.canva.com/design/DAEoT0cl6mY/XIlQ8gdNusCwWlsIFIBOnA/view?utm_content=DAEoT0cl6mY&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton"
-              target="_blank"
-              startIcon={<SaveAltIcon />}
-            >
-              Download CV
-            </Button>
+            <p>
+              Hello, I'm Dolly, a frontend developer based in Bangalore, India.
+              I'm passionate about working with new technologies and developing
+              innovative applications. I am also patinate for photography, when
+              i find free time I contribute photos to a comunity called
+              Mixphotos. I have Youtube Channel where I upload my project's
+              video and many more things.
+            </p>
+            <br />
+            <p className="homeInfo--info1--p2">
+              Have a Look at My
+              <a
+                href="https://www.youtube.com/channel/UCD4YW5rhsXi-BK2tzbWVIZA"
+                style={{ color: "white" }}
+              >
+                {" "}
+                Youtube{" "}
+              </a>
+              Channel, It will Students/developer to learn thing in easy ways
+              and help you to make projects. Or you can connect me on Facebook,
+              Linkdin or Instagram. I am always excited to talk about Tech.
+            </p>
+            <br />
+            <div className="homeInfo--info--contact--btn">
+              <Button
+                size="large"
+                className="homeInfo--info--contact"
+                href="tel: 6300810401"
+                endIcon={<ArrowRightAltIcon />}
+              >
+                Contact me
+              </Button>
+              <Button
+                variant="outlined"
+                size="large"
+                className="homeInfo--info--CV"
+                href="https://www.canva.com/design/DAEoT0cl6mY/XIlQ8gdNusCwWlsIFIBOnA/view?utm_content=DAEoT0cl6mY&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton"
+                target="_blank"
+                startIcon={<SaveAltIcon />}
+              >
+                Download CV
+              </Button>
             </div>
           </div>
 
